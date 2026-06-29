b98ce9cd3d5fdd257688cfc0f06de39dac26ad77
nanababonsu1 <ohenenana_n@yahoo.com>
2026-06-29 10:44:32 -0400

    Add info display for active rainfall scenario

--- app.py
+++ app.py
@@ -261,6 +261,9 @@ elif rainfall_scenario == "Extreme Flooding Scenario":
     rainfall_input = 300
 else:
     rainfall_input = rainfall
+    st.sidebar.info(
+    f"🌧 Active Scenario: {rainfall_scenario}\n\nRainfall Input: {rainfall_input} mm"
+)
 water_access = st.sidebar.slider("Water Access (%)", 0, 100, int(selected["Water_Access"]))
 sanitation = st.sidebar.slider("Sanitation Coverage (%)", 0, 100, int(selected["Sanitation"]))
 population_density = st.sidebar.slider("Population Density", 100, 6000, int(selected["Population_Density"]))
