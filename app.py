1b7138fcbea4b389189ee43c551f15598038add4
nanababonsu1 <ohenenana_n@yahoo.com>
2026-07-04 11:01:23 -0400

    Add 'Live Weather - Accra' to scenarios list

--- app.py
+++ app.py
@@ -266,6 +266,7 @@ rainfall_scenario = st.sidebar.selectbox(
         "Moderate Rainfall Scenario",
         "Heavy Rainfall Scenario",
         "Extreme Flooding Scenario"
+        "Live Weather - Accra"
     ]
 )
 
